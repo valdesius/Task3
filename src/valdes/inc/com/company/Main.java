d4a37c75c135377b83cb60e37b820f9d264b3b09
valdesius <v.inc@inbox.ru>
2021-10-01 16:46:54 +0300

    V1.02

--- src/valdes/inc/com/company/Main.java
+++ src/valdes/inc/com/company/Main.java
@@ -1,28 +1,29 @@
 package valdes.inc.com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
     }
-    public static final Line L1 = new Line(-1, 6, 5.0 / 2);
-    public static final HorizontalParabola P1 =
-            new HorizontalParabola(2, 2, 1);
+
+    public static double readDouble(String name) {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("input %s = ", name);
+        return in.nextDouble();
+    }
+
+    public static final Line L1 = new Line(-5, 0, 2);
+    public static final Parabola P1 = new Parabola();
+    public static final Parabola P2 = new Parabola();
+    public static final HorizontalParabola HP1 = new HorizontalParabola(3, -1, 1.0 / 8);
+
     public static SimpleColor getColor(double x, double y) {
-        // реализовать самостоятельно
-        if (P1.isPointRightOfParabola(x, y)) {
-            return SimpleColor.ORANGE;
-        }
-        if (y < -2 && L1.isPointAboveLine(x, y)) {
-            return SimpleColor.GREEN;
-        }
-        if (y < -2 && !L1.isPointAboveLine(x, y)) {
-            return SimpleColor.YELLOW;
-        }
-        return SimpleColor.GRAY;
+
+        return SimpleColor.WHITE;
     }
 
-    public static void printColorForPoint(double x, double y) {
 
-    }
 }
+
