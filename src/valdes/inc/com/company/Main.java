cce11c89d163c4c175fef0ef5244164bbd20c855
valdesius <v.inc@inbox.ru>
2021-10-01 16:54:35 +0300

    V1.03

--- src/valdes/inc/com/company/Main.java
+++ src/valdes/inc/com/company/Main.java
@@ -5,6 +5,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Point p1 = new Point(-6, -6);
+        System.out.println("(" + p1.x + ", " + p1.y + ") -->" + getColor(p1.x, p1.y));
+
+        Point p2 = new Point(-6, 0);
+        System.out.println("(" + p2.x + ", " + p2.y + ") -->" + getColor(p2.x, p2.y));
+
+        Point p3 = new Point(0, 0);
+        System.out.println("(" + p3.x + ", " + p3.y + ") -->" + getColor(p3.x, p3.y));
+
+        Point p4 = new Point(0, 8);
+        System.out.println("(" + p4.x + ", " + p4.y + ") -->" + getColor(p4.x, p4.y));
+
+        Point p5 = new Point(-1, 4);
+        System.out.println("(" + p5.x + ", " + p5.y + ") -->" + getColor(p5.x, p5.y));
+
+        Point p6 = new Point(4, 0);
+        System.out.println("(" + p6.x + ", " + p6.y + ") -->" + getColor(p6.x, p6.y));
+
+        Point p7 = new Point(readDouble("x"), readDouble("y"));
+        System.out.println("(" + p7.x + ", " + p7.y + ") -->" + getColor(p7.x, p7.y));
 
     }
 
@@ -14,9 +34,10 @@ public class Main {
         return in.nextDouble();
     }
 
+
     public static final Line L1 = new Line(-5, 0, 2);
-    public static final Parabola P1 = new Parabola();
-    public static final Parabola P2 = new Parabola();
+    public static final Parabola P1 = new Parabola(-6, -5, -1.0 / 4);
+    public static final Parabola P2 = new Parabola(-2, 1, 1);
     public static final HorizontalParabola HP1 = new HorizontalParabola(3, -1, 1.0 / 8);
 
     public static SimpleColor getColor(double x, double y) {
--- src/valdes/inc/com/company/Parabola.java
+++ src/valdes/inc/com/company/Parabola.java
@@ -1,4 +1,24 @@
 package valdes.inc.com.company;
 
+
 public class Parabola {
+    public double x0;
+    public double y0;
+    public double a;
+
+    public Parabola(double x0, double y0, double a) {
+        this.x0 = x0;
+        this.y0 = y0;
+        this.a = a;
+    }
+
+    /**
+     * Проверяет, находится ли точка (x, y) справа
+     * (снизу, если повернуть изобюражение на 90 градусов
+     * против часовой стрелки)
+     * от параболы
+     */
+    public boolean isPointDownOfParabola(double x, double y) {
+        return y < a * Math.pow(x - x0, 2) + y0;
+    }
 }
